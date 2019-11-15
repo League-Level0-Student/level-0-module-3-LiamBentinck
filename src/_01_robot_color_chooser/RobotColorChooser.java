c99f81e726625587881ecd72d3b5a5f184b0a44a
League Student <league@iMac-17.attlocal.net>
2019-11-14 16:59:08 -0800

    Finished module 2

--- src/_01_robot_color_chooser/RobotColorChooser.java
+++ src/_01_robot_color_chooser/RobotColorChooser.java
@@ -4,12 +4,16 @@
 package _01_robot_color_chooser;
 
 import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+
 import java.awt.Color;
 
 public class RobotColorChooser {
 	public static void main(String[] args) {
 		//1. Create a new Robot
-		
+		 Robot rob = new Robot();
+				 
 		//3. Ask the user what color they would like the robot to draw
 		
 		//5. Use an if/else statement to set the pen color that the user requested
@@ -21,7 +25,14 @@ public class RobotColorChooser {
 		//4. Set the pen width to 10
 		
 	    //2. Make the robot draw a shape (this will take more than one line of code)
-
-
+		 rob.penDown();
+		 rob.move(100);
+		 rob.turn(90);
+		 rob.move(100);
+		 rob.turn(90);
+		 rob.move(100);
+		 rob.turn(90);
+		 rob.move(100);
+		 rob.turn(90);
 	}
 }
